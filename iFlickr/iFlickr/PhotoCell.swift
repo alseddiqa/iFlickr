edfcb9cb21176d02ab017f44bb3fedba5fa035a2
Abdullah <alseddiq.af@gmail.com>
2020-11-29 18:58:25 +0300

    Photos Collection View
    Added photo cell components
    -created a cell class

---
+++ iFlickr/iFlickr/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  iFlickr
+//
+//  Created by Abdullah Alseddiq on 11/29/20.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    @IBOutlet var imageView: UIImageView!
+}
